a60dc58f0c6587168d416765a2ffb60894b2d817
JCCG Code <jccuadradogracia@gmail.com>
2025-07-03 12:58:48 +0200

    cors applied

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 // Packages
 import express from 'express'
+import cors from 'cors'
 // Local files
 import recipeRoutesV1 from './api/v1/routes/recipe.routes.js'
 
+// Initializacions
 const app = express()
+const corsOptions = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204
+}
 
+app.use(cors(corsOptions))
 app.use(express.json())
 
 app.use('/api/v1/recipes', recipeRoutesV1)
